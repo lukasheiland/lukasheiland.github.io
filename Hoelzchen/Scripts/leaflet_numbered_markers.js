df964f0ba4eeb22db74ca6051221e7bcea97f92f
Lukas Heiland <lukas.heiland@gmail.com>
2018-09-08 00:24:46 +0200

    bjk

--- Hoelzchen/Scripts/leaflet_numbered_markers.js
+++ Hoelzchen/Scripts/leaflet_numbered_markers.js
@@ -1,7 +1,7 @@
 L.NumberedDivIcon = L.Icon.extend({
 	options: {
     // EDIT THIS TO POINT TO THE FILE AT http://www.charliecroom.com/marker_hole.png (or your own marker)
-    iconUrl: '<%= image_path("http://www.charliecroom.com/marker_hole.png") %>',
+    iconUrl: '<%= image_path("Scripts/marker_hole.png") %>',
     number: '',
     shadowUrl: null,
     iconSize: new L.Point(25, 41),
